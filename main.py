a022a4784105d185ace46e0e23445a77a5485a76
cwj6676 <csi774912@gmail.com>
2026-09-19 12:38:40 +0000

    Add random topology selection

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-print("NetFaaultLab started")
-
 import random
 
 faults = [
@@ -7,11 +5,18 @@ faults = [
     "Wrong IP Address",
     "Wrong Default Gateway",
     "Wrong Static Route",
-    "ACL Blook"
+    "ACL Block"
+]
+
+topologies = [
+    "Small Office",
+    "Branch Network",
+    "Multi VLAN"
 ]
 
 selected_fault = random.choice(faults)
+selected_topology = random.choice(topologies)
 
-print("NetFaultLab started")
-print("Selected fault:, selected_fault)
-      
+print("NetFaultLab Started")
+print("Selected fault:", selected_fault)
+print("Selected topology:", selected_topology)
