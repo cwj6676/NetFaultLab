d03676c44bc7f4714ac8d63e84046df84076074d
cwj6676 <csi774912@gmail.com>
2026-09-19 13:26:42 +0000

    Fault 랜덤 선택 장비 수 랜덤 결정 장비 이름 생성

--- main.py
+++ main.py
@@ -20,3 +20,51 @@ selected_topology = random.choice(topologies)
 print("NetFaultLab Started")
 print("Selected fault:", selected_fault)
 print("Selected topology:", selected_topology)
+
+if selected_topology == "Small Office":
+    topology_map = "PC - Switch - Router - Server"
+    routers = random.randint(1, 2)
+    switches = random.randint(1, 2)
+    clients = random.randint(2, 6)
+    servers = random.randint(1,2)
+
+elif selected_topology == "Branch Network":
+    topology_map = "PC = Switch - R1 - R2 - R3 - Server"
+    routers = random.randint(3, 5)
+    switches = random.randint(1, 3)
+    clients = random.randint(2, 8)
+    servers = random.randint(1,2)
+
+elif selected_topology == "Multi VLAN":
+    topology_map = "PC1/PC2/PC3 - Switch - Router - Server"
+    routers = random.randint(1, 2)
+    switches = random.randint(2, 4)
+    clients = random.randint(4, 12)
+    servers = random.randint(1,3)
+
+router_names = []
+for i in range(1, routers + 1):
+    router_names.append("R" + str(i))
+
+client_names = []
+for i in range(1, clients + 1):
+    client_names.append("PC" + str(i))
+
+switch_names = []
+for i in range(1, switches + 1):
+    switch_names.append("SW" + str(i))
+
+servers_names = []
+for i in range(1, servers + 1):
+    servers_names.append("Server" + str(i))
+
+print("topology_map:", topology_map)
+print("Routers :", routers)
+print("Switches :", switches)
+print("Clients :", clients)
+print("servers :", servers)
+print("Routers :", router_names)
+print("Switches :", switch_names)
+print("Clients :", client_names)
+print("Servers :", servers_names)
+
