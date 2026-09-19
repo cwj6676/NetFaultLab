320904638d9a980ff2382a71c43fca060f3bc53e
cwj6676 <csi774912@gmail.com>
2026-09-19 14:50:07 +0000

    R1 LAN IP / Default Gateway 생성 라우터 간 주소 자동 생성 Multi VLAN 소속 정보 링크에 포함

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+# 장애 종류
 faults = [
     "Interface Down",
     "Wrong IP Address",
@@ -8,6 +9,7 @@ faults = [
     "ACL Block"
 ]
 
+# 네트워크 종류
 topologies = [
     "Small Office",
     "Branch Network",
@@ -21,27 +23,29 @@ print("NetFaultLab Started")
 print("Selected fault:", selected_fault)
 print("Selected topology:", selected_topology)
 
+# 토폴로지별 장비 수
 if selected_topology == "Small Office":
     topology_map = "PC - Switch - Router - Server"
     routers = random.randint(1, 2)
     switches = random.randint(1, 2)
     clients = random.randint(2, 6)
-    servers = random.randint(1,2)
+    servers = random.randint(1, 2)
 
 elif selected_topology == "Branch Network":
-    topology_map = "PC = Switch - R1 - R2 - R3 - Server"
+    topology_map = "PC - Switch - R1 - R2 - R3 - Server"
     routers = random.randint(3, 5)
     switches = random.randint(1, 3)
     clients = random.randint(2, 8)
-    servers = random.randint(1,2)
+    servers = random.randint(1, 2)
 
 elif selected_topology == "Multi VLAN":
     topology_map = "PC1/PC2/PC3 - Switch - Router - Server"
     routers = random.randint(1, 2)
     switches = random.randint(2, 4)
     clients = random.randint(4, 12)
-    servers = random.randint(1,3)
+    servers = random.randint(1, 3)
 
+# 장비 이름 생성
 router_names = []
 for i in range(1, routers + 1):
     router_names.append("R" + str(i))
@@ -57,15 +61,31 @@ for i in range(1, switches + 1):
 servers_names = []
 for i in range(1, servers + 1):
     servers_names.append("Server" + str(i))
+
+# PC쪽 네트워크
 lan_number = random.randint(1, 254)
 lan_network = "192.168." + str(lan_number)
 
 client_ips = []
+router_lan_ip = lan_network + ".1"
+default_gateway = router_lan_ip
 
 for i in range(len(client_names)):
     ip = lan_network + "." + str(i + 10)
     client_ips.append(ip)
-    
+
+# 라우터 사이 IP
+router_links = []
+
+for i in range(len(router_names) - 1):
+    network_number = i + 1
+
+    left_ip = "10.0." + str(network_number) + ".1"
+    right_ip = "10.0." + str(network_number) + ".2"
+
+    router_links.append((router_names[i], left_ip, router_names[i + 1], right_ip))
+
+# 장비 연결
 links = []
 
 if selected_topology == "Small Office":
@@ -78,7 +98,7 @@ if selected_topology == "Small Office":
 
     for i in range(len(router_names) - 1):
         links.append((router_names[i], router_names[i + 1]))
-    
+
     for server in servers_names:
         links.append((router_names[-1], server))
 
@@ -86,13 +106,13 @@ elif selected_topology == "Branch Network":
     for i in range(len(client_names)):
         switch_index = i % len(switch_names)
         links.append((client_names[i], switch_names[switch_index]))
-    
+
     for switch in switch_names:
         links.append((switch, "R1"))
 
     for i in range(len(router_names) - 1):
         links.append((router_names[i], router_names[i + 1]))
-    
+
     for server in servers_names:
         links.append((router_names[-1], server))
 
@@ -110,15 +130,24 @@ elif selected_topology == "Multi VLAN":
     for switch in switch_names:
         links.append((switch, "R1"))
 
-    for server in servers_names:
-        links.append(("R1", server))
+    for i in range(len(router_names) - 1):
+        links.append((router_names[i], router_names[i + 1]))
 
+    for server in servers_names:
+        links.append((router_names[-1], server))
+
+# 결과 출력
 print("\n=== Network Links ===")
 
 for link in links:
     print(link)
 
 print("topology_map:", topology_map)
+print("\n=== Router Links ===")
+
+for link in router_links:
+    print(link)
+
 print("Routers :", routers)
 print("Switches :", switches)
 print("Clients :", clients)
@@ -128,4 +157,5 @@ print("Switches :", switch_names)
 print("Clients :", client_names)
 print("Client IPs:", client_ips)
 print("Servers :", servers_names)
-
+print("Router LAN IP :", router_lan_ip)
+print("Default Gateway :", default_gateway)
