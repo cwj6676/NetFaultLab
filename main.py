d4604b9bed27be3ccea1cd0d2b599b997db4a1ee
cwj6676 <csi774912@gmail.com>
2026-09-19 20:36:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 print("NetFaaultLab started")
+
+import random
+
+faults = [
+    "Interface Down",
+    "Wrong IP Address",
+    "Wrong Default Gateway",
+    "Wrong Static Route",
+    "ACL Blook"
+]
+
+selected_fault = random.choice(faults)
+
+print("NetFaultLab started")
+print("Selected fault:, selected_fault)
+      
